df65f1a6d622e6544c4d73f29f1092455f9b726e
Dav Singh <davinder.cheema@prodyna.com>
2021-04-14 10:55:37 +0100

    [CHORE] Added basic materials designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography/Typography";
 
 const App:React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const increment = () => setCount(prevState => prevState + 1);
+  const decrement = () => setCount(prevState => prevState - 1);
   return (
-    <>
-      <button onClick={increment}>Click Me</button>
-      <div>Count {count}</div>
-    </>
+    <Grid
+        container
+        spacing={3}
+        direction={"column"}
+        alignItems={"center"}
+        justify={"center"}
+    >
+        <Grid item xs={12}>
+            <Typography variant="h1">Simple React Counter</Typography>
+        </Grid>
+        <Grid item xs={12}>
+            <Button variant="contained" color="primary" onClick={increment}>
+                +
+            </Button>
+            <Typography align={"center"}>{count}</Typography>
+            <Button variant="contained" color="primary" onClick={decrement}>
+                -
+            </Button>
+        </Grid>
+    </Grid>
   );
 };
 
