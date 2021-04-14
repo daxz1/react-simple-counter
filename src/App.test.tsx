b2f3d2501232127d32dc93c71a53229c02ef4c1a
Dav Singh <davinder.cheema@prodyna.com>
2021-04-14 11:16:29 +0100

    [CHORE] Added basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Simple React Counter/i);
   expect(linkElement).toBeInTheDocument();
 });
