a4b2770cdf3a3c886644b83b34994ef283ecc58b
Dav Singh <davinder.cheema@prodyna.com>
2021-04-14 10:37:38 +0100

    [CHORE] Added simple counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App:React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const increment = () => setCount(prevState => prevState + 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={increment}>Click Me</button>
+      <div>Count {count}</div>
+    </>
   );
-}
+};
 
 export default App;
